f9f13ea311125e1c07dfe3c042971e1140434d87
yagolimalins <yago.lima.lins@protonmail.com>
2020-06-16 06:11:43 -0300

    Bug do email duplicado resolvido, script melhor estruturado e banner modificado

--- OLXCRAPPER.py
+++ OLXCRAPPER.py
@@ -1,30 +1,45 @@
-import requests
-import time
-from bs4 import BeautifulSoup
 import os
+import time
 import smtplib
+import requests
+from getpass import getpass
+from bs4 import BeautifulSoup
 from email.message import EmailMessage
 
-url = "https://sp.olx.com.br/celulares" # <- MUDE O LINK DE ACORDO COM A CATEGORIA E ESTADO DESEJADOS 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'} 
+# DECLARAÇÃO E INICIALIZAÇÃO DE VARIÁVEIS:
 
-EMAIL_ADDRESS = 'xxxxxx@gmail.com' # <- INSIRA SEU GMAIL ENTRE ASPAS COMO NO EXEMPLO
-EMAIL_PASSWORD = 'xxxxxxxxxxxxxxxx' # <- INSIRA SUA SENHA ENTRE ASPAS COMO NO EXEMPLO
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER') 
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
-# Alternativamente você pode usar as variáveis de ambiente abaixo caso as mesmas estejam configuradas no sistema
+url = 'https://www.olx.com.br/celulares'
 
-#EMAIL_ADDRESS = os.environ.get('EMAIL_USER') 
-#EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}
+timesleep = 15
+
+# DECLARAÇÃO DE FUNÇÕES:
 
 def banner():
 
     print()
-    print(" ██████╗ ██╗    ██╗     ██╗  ██╗      ██████╗██████╗  █████╗ ██████╗ ██████╗ ███████╗██████╗")
-    print("██╔═══██╗██║    ╚═╝      ██╗██╔╝     ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗")
-    print("██║   ██║██║              ███╔╝█████╗██║     ██████╔╝███████║██████╔╝██████╔╝█████╗  ██████╔╝")
-    print("██║   ██║██║             ██╔██╗╚════╝██║     ██╔══██╗██╔══██║██╔═══╝ ██╔═══╝ ██╔══╝  ██╔══██╗")
-    print("╚██████╔╝███████╗       ██╔╝ ██╗     ╚██████╗██║  ██║██║  ██║██║     ██║     ███████╗██║  ██║")
-    print(" ╚═════╝ ╚══════╝       ╚═╝  ╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚══════╝╚═╝  ╚═╝ \n")
+    print("╔═══════════════════════════════════════════════════════════════════════════════════════════════╗")
+    print("║  ██████╗ ██╗    ██╗     ██╗  ██╗      ██████╗██████╗  █████╗ ██████╗ ██████╗ ███████╗██████╗  ║")
+    print("║ ██╔═══██╗██║    ╚═╝      ██╗██╔╝     ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗ ║")
+    print("║ ██║   ██║██║              ███╔╝█████╗██║     ██████╔╝███████║██████╔╝██████╔╝█████╗  ██████╔╝ ║")
+    print("║ ██║   ██║██║             ██╔██╗╚════╝██║     ██╔══██╗██╔══██║██╔═══╝ ██╔═══╝ ██╔══╝  ██╔══██╗ ║")
+    print("║ ╚██████╔╝███████╗       ██╔╝ ██╗     ╚██████╗██║  ██║██║  ██║██║     ██║     ███████╗██║  ██║ ║")
+    print("║  ╚═════╝ ╚══════╝       ╚═╝  ╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚══════╝╚═╝  ╚═╝ ║")
+    print("╚═══════════════════════════════════════════════════════════════════════════════════════════════╝")
+    print("  Ferramenta de busca por novos anúncios em categorias da OLX com notificações por email (GMail) ")
+    print("Autor: Yago Lima Lins | yago.lima.lins@protonmail.com | https://github.com/yagolimalins/OLXCRAPPER")
+    print("-------------------------------------------------------------------------------------------------\n")
+
+
+def screen_clear():
+   if os.name == 'posix':
+      _ = os.system('clear')
+   else:
+      _ = os.system('cls')
+
 
 def statuscode(url=url, headers=headers):
 
@@ -43,52 +58,59 @@ def webscrap(url=url, headers=headers, ):
 
     for item in items:
 
-            if "R$" in item.text:
-                title = item.get('title')
-                price = item.find('p').get_text()
-                url = item.attrs['href']
+            if ("Hoje" and "R$") in item.text:
+
+                    title = item.get('title')
+                    price = item.find('p').get_text()
+                    url = item.attrs['href']
+                    lista.append([title, price, url])
+
+                    print(price + ' - ' + title)
         
-                print(price + ' - ' + title)
-        
-                lista.append([title, price, url])
+    
+    print("\n-------------------------------------------------------------------------------------------------"
 
     return(lista)
 
-def sendmail(lista, EMAIL_ADDRESS=EMAIL_ADDRESS, EMAIL_PASSWORD=EMAIL_PASSWORD):
+# EXECUÇÃO DO SCRIPT:
 
-    msg = EmailMessage()
-    msg['Subject'] = "[OLXCRAPPER] Novo item: " + lista[0][0] + " - " + lista[0][1]
-    msg['From'] = EMAIL_ADDRESS
-    msg['To'] = EMAIL_ADDRESS
-    msg.set_content('Link para o anuncio: ' + lista[0][2])
+banner()
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+listaOld = webscrap()
+print("O item mais recentemente adicionado é: " + listaOld[0][0] + " - " + listaOld[0][1])
 
-        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-
-        smtp.send_message(msg)
-
-def checkandsend(listaOld):
-    listaNew = webscrap()
-    if listaNew[0][0] != listaOld[0][0]:
-        print("\n--------------------------------------------------------------------------------------")
-        print("O item mais recentemente anunciado é: " + listaNew[0][0] + " - " + listaNew[0][1])
-        sendmail(listaNew)
-        global old 
-        old = listaNew
-    elif listaNew[0][0] == listaOld[0][0]:
-        print("\n--------------------------------------------------------------------------------------")
-        print("O item mais recentemente anunciado continua sendo: " + old[0][0] + " - " + old[0][1])
-    
-old = webscrap()
+time.sleep(timesleep)
 
 while True:
-    
-    banner()
-    statuscodenumber = statuscode()
-    if statuscodenumber == 200:
-        checkandsend(old)
-    else:
-        print("Erro de conexão, código de status do site: " + str(statuscodenumber))
 
-    time.sleep(10)
+    screen_clear()
+
+    banner()
+    
+    statuscodenumber = statuscode()
+
+    if statuscodenumber == 200:
+        listaNew = webscrap()
+
+        if listaNew[0][0] != listaOld[0][0]:
+            print("O item mais recentemente anunciado é: " + listaNew[0][0] + " - " + listaNew[0][1])
+            
+            msg = EmailMessage()
+            msg['Subject'] = "[OLXCRAPPER] " + listaNew[0][0] + " - " + listaNew[0][1]
+            msg['From'] = EMAIL_ADDRESS
+            msg['To'] = EMAIL_ADDRESS
+            msg.set_content('Link para o anuncio: ' + listaNew[0][2])
+
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                smtp.send_message(msg)
+
+            listaOld = listaNew
+        
+        elif listaNew[0][0] == listaOld[0][0]:
+            print("O item mais recentemente anunciado continua sendo: " + listaOld[0][0] + " - " + listaOld[0][1])
+    
+    else:
+        print("O site retornou código de status: " + str(statuscodenumber))
+
+    time.sleep(timesleep)
