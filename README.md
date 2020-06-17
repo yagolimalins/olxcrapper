0542dfa7d8c23c379e5850937e628c9b60054052
yagolimalins <yago.lima.lins@protonmail.com>
2020-06-17 09:36:21 -0300

    Argumento adicionado para o timesleep

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-<h1 align="center">OL' X-CRAPPER
-<p>
-  <img alt="Version" src="https://img.shields.io/badge/version-0.1.0-blue.svg?cacheSeconds=2592000" />
-  <a href="https://www.gnu.org/licenses/gpl-3.0.pt-br.html" target="_blank">
-    <img alt="License: GNU" src="https://img.shields.io/badge/License-GNU-yellow.svg" />
-  </a>
-</p>
-
-</h1>
-
-### [Homepage](https://github.com/yagolimalins/olxcrapper)
+### [OLXCRAPPER](https://github.com/yagolimalins/olxcrapper)
 
 ## Introdução
 
@@ -24,26 +14,15 @@ O usuário tem que usar uma conta do GMail, por enquanto é o unico servidor sup
 
 ## Instalação e uso
 
-Certifique-se de que tem instalados o git e python instalados, após isso installe as dependências 
-requeridas pelo script utilizando os seguintes comandos no terminal:
-
 ```sh
-python -m pip install bs4 requests lxml argparse
-```
-
-Após isso você já pode clonar o repositório e utilizar o script como exemplificado abaixo:
-
-```sh
-git clone https://github.com/yagolimalins/olxcrapper.git
-cd olxcrapper
-python ./olxcrapper.py -g seuemail@gmail.com -s senhadogmail -u https://sp.olx.com.br/celulares
+olxcrapper -g seuemail@gmail.com -s senhadogmail -u https://sp.olx.com.br/celulares -t 15
 ```
 
 Substitua os dados de login acima com suas credenciais do gmail e com o link da categoria desejada.
 
 * Quanto a conta do GMail, sugiro criar uma nova conta pra uso desse script para que sua caixa de entrada
-  não seja floodada com emails do tipo, é importante que na conta do GMail utilizada esteja habilitada
-a opção de acesso de apps menos confiáveis.
+  não seja floodada com emails do tipo, é importante que na conta do GMail utilizada esteja habilitada 
+  a opção de acesso de apps menos confiáveis.
 
 * Caso deseje mais segurança ao utilizar sua conta, habilite
   a opção de autenticação de dois fatores e crie uma senha exclusiva para o uso desse script, esse tipo de senha
@@ -51,9 +30,9 @@ a opção de acesso de apps menos confiáveis.
   uma senha de app aqui: https://security.google.com/settings/security/apppasswords
 
 * Quanto ao link da categoria da olx desejada, para obter um link utilizável, acesse a categoria da olx desejada
-escolha seu estado e/ou cidade e copie o link do navegador pra substituir no comando exemplificado acima.
+  escolha seu estado e/ou cidade e copie o link do navegador pra substituir no comando exemplificado acima.
 
-* O script irá executar continuamente e irá atualizar a lista a cada 5 segundos, sempre que encontrar um
+* O script irá executar continuamente e irá atualizar a lista a cada -t segundos, sempre que encontrar um
   anúncio novo na categoria referida pelo usuário, o mesmo enviará um email para a caixa de entrada do GMail indicado,
   esse script só enviará email caso haja um anuncio novo, assim evitando floodar a caixa de entrada.
 
@@ -69,20 +48,18 @@ a listagem de anúncios mas não conseguirá notificar por email.
 
 ## Contribua com o projeto
 
-Contribuições, bugs e pedidos de novas funcionalidades são bem vindos! <br />
+Contribuições, bugs e pedidos de novas funcionalidades são bem vindos!
 Sinta-se a vontade para conferir: [issues page](https://github.com/yagolimalins/olxcrapper/issues). 
 
 ## Apoie o autor
 
-Deixe uma estrela se esse projeto te ajudou! :)
+Considere fazer uma contribuição no patreon caso esse script seja útil pra você :)
 
-<a href="https://www.patreon.com/yagolimalins">
-  <img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160">
-</a>
+**https://www.patreon.com/yagolimalins**
 
 ## Licença
 
-Copyright © 2020 [Yago Lima Lins](https://github.com/yagolimalins).<br />
+Copyright © 2020 [Yago Lima Lins](https://github.com/yagolimalins).
 This project is [GNU](https://www.gnu.org/licenses/gpl-3.0.pt-br.html) licensed.
 
 ***
--- olxcrapper.py
+++ olxcrapper.py
@@ -7,122 +7,127 @@ from getpass import getpass
 from bs4 import BeautifulSoup
 from email.message import EmailMessage
 
+def main():
 
-# DECLARAÇÃO E INICIALIZAÇÃO DE VARIÁVEIS:
-
-
-parser = argparse.ArgumentParser(description='Dados do GMail e URL da categoria')
-parser.add_argument('-u', '--URL', type=str, metavar='', required=True, help='Link da categoria da OLX (ex: https://sp.olx.com.br/celulares')
-parser.add_argument('-g', '--GMAIL', type=str, metavar='', required=True, help='Endereço do GMail')
-parser.add_argument('-s', '--SENHA', type=str, metavar='', required=True, help='Senha do GMail')
-
-args = parser.parse_args()
-
-URL = args.URL
-EMAIL_ADDRESS = args.GMAIL
-EMAIL_PASSWORD = args.SENHA
-
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}
-timesleep = 5
+    # DECLARAÇÃO E INICIALIZAÇÃO DE VARIÁVEIS:
 
 
-# DECLARAÇÃO DE FUNÇÕES:
+    parser = argparse.ArgumentParser(description='Dados do GMail e URL da categoria')
+    parser.add_argument('-u', '--url', type=str, metavar='', required=True, help='Link da categoria da OLX (ex: https://sp.olx.com.br/celulares')
+    parser.add_argument('-g', '--gmail', type=str, metavar='', required=True, help='Endereço do GMail')
+    parser.add_argument('-s', '--senha', type=str, metavar='', required=True, help='Senha do GMail')
+    parser.add_argument('-t', '--timesleep', type=str, metavar='', required=True, help='Tempo entre atualizações')
+
+    args = parser.parse_args()
+
+    URL = args.url
+    EMAIL_ADDRESS = args.gmail
+    EMAIL_PASSWORD = args.senha
+    timesleep = args.timesleep
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}
 
 
-def banner():
-
-    print("╔═══════════════════════════════════════════════════════════════════════════════════════════════╗")
-    print("║  ██████╗ ██╗    ██╗     ██╗  ██╗      ██████╗██████╗  █████╗ ██████╗ ██████╗ ███████╗██████╗  ║")
-    print("║ ██╔═══██╗██║    ╚═╝      ██╗██╔╝     ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗ ║")
-    print("║ ██║   ██║██║              ███╔╝█████╗██║     ██████╔╝███████║██████╔╝██████╔╝█████╗  ██████╔╝ ║")
-    print("║ ██║   ██║██║             ██╔██╗╚════╝██║     ██╔══██╗██╔══██║██╔═══╝ ██╔═══╝ ██╔══╝  ██╔══██╗ ║")
-    print("║ ╚██████╔╝███████╗       ██╔╝ ██╗     ╚██████╗██║  ██║██║  ██║██║     ██║     ███████╗██║  ██║ ║")
-    print("║  ╚═════╝ ╚══════╝       ╚═╝  ╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚══════╝╚═╝  ╚═╝ ║")
-    print("╚═══════════════════════════════════════════════════════════════════════════════════════════════╝")
-    print("  Ferramenta de busca por novos anúncios em categorias da OLX com notificações por email (GMail) ")
-    print("Autor: Yago Lima Lins | yago.lima.lins@protonmail.com | https://github.com/yagolimalins/olxcrapper")
-    print("-------------------------------------------------------------------------------------------------\n")
+    # DECLARAÇÃO DE FUNÇÕES:
 
 
-def screen_clear():
-   if os.name == 'posix':
-      _ = os.system('clear')
-   else:
-      _ = os.system('cls')
+    def banner():
+
+        print("╔═══════════════════════════════════════════════════════════════════════════════════════════════╗")
+        print("║  ██████╗ ██╗    ██╗     ██╗  ██╗      ██████╗██████╗  █████╗ ██████╗ ██████╗ ███████╗██████╗  ║")
+        print("║ ██╔═══██╗██║    ╚═╝      ██╗██╔╝     ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗ ║")
+        print("║ ██║   ██║██║              ███╔╝█████╗██║     ██████╔╝███████║██████╔╝██████╔╝█████╗  ██████╔╝ ║")
+        print("║ ██║   ██║██║             ██╔██╗╚════╝██║     ██╔══██╗██╔══██║██╔═══╝ ██╔═══╝ ██╔══╝  ██╔══██╗ ║")
+        print("║ ╚██████╔╝███████╗       ██╔╝ ██╗     ╚██████╗██║  ██║██║  ██║██║     ██║     ███████╗██║  ██║ ║")
+        print("║  ╚═════╝ ╚══════╝       ╚═╝  ╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚══════╝╚═╝  ╚═╝ ║")
+        print("╚═══════════════════════════════════════════════════════════════════════════════════════════════╝")
+        print("  Ferramenta de busca por novos anúncios em categorias da OLX com notificações por email (GMail) ")
+        print("Autor: Yago Lima Lins | yago.lima.lins@protonmail.com | https://github.com/yagolimalins/olxcrapper")
+        print("-------------------------------------------------------------------------------------------------\n")
 
 
-def statuscode(URL=URL, headers=headers):
+    def screen_clear():
+        if os.name == 'posix':
+            _ = os.system('clear')
+        else:
+            _ = os.system('cls')
 
-    result = requests.get(URL, headers=headers)
-    statuscodenumber = int(result.status_code)
 
-    return(statuscodenumber)
+    def statuscode(URL=URL, headers=headers):
 
-def webscrap(URL=URL, headers=headers, ):
+        result = requests.get(URL, headers=headers)
+        statuscodenumber = int(result.status_code)
 
-    result = requests.get(URL, headers=headers)
-    src = result.content
-    soup = BeautifulSoup(src, 'lxml')
-    items = soup.find_all('a')
-    lista = []
+        return(statuscodenumber)
 
-    for item in items:
+    def webscrap(URL=URL, headers=headers, ):
 
-            if ("Hoje" and "R$") in item.text:
+        result = requests.get(URL, headers=headers)
+        src = result.content
+        soup = BeautifulSoup(src, 'lxml')
+        items = soup.find_all('a')
+        lista = []
 
-                    title = item.get('title')
-                    price = item.find('p').get_text()
-                    URL = item.attrs['href']
-                    lista.append([title, price, URL])
+        for item in items:
 
-                    print(price + ' - ' + title)
+                if ("Hoje" and "R$") in item.text:
+
+                        title = item.get('title')
+                        price = item.find('p').get_text()
+                        URL = item.attrs['href']
+                        lista.append([title, price, URL])
+
+                        print(price + ' - ' + title)
+            
         
-    
-    print("\n-------------------------------------------------------------------------------------------------")
+        print("\n-------------------------------------------------------------------------------------------------")
 
-    return(lista)
-
-
-# EXECUÇÃO DO SCRIPT:
+        return(lista)
 
 
-banner()
+    # EXECUÇÃO DO SCRIPT:
 
-listaOld = webscrap()
-print("O item mais recentemente adicionado é: " + listaOld[0][0] + " - " + listaOld[0][1])
-
-time.sleep(timesleep)
-
-while True:
-
-    screen_clear()
 
     banner()
-    
-    statuscodenumber = statuscode()
 
-    if statuscodenumber == 200:
-        listaNew = webscrap()
-
-        if listaNew[0][0] != listaOld[0][0]:
-            print("O item mais recentemente anunciado é: " + listaNew[0][0] + " - " + listaNew[0][1])
-            
-            msg = EmailMessage()
-            msg['Subject'] = "[OLXCRAPPER] " + listaNew[0][0] + " - " + listaNew[0][1]
-            msg['From'] = EMAIL_ADDRESS
-            msg['To'] = EMAIL_ADDRESS
-            msg.set_content('Link para o anuncio: ' + listaNew[0][2])
-
-            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-                smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-                smtp.send_message(msg)
-
-            listaOld = listaNew
-        
-        elif listaNew[0][0] == listaOld[0][0]:
-            print("O item mais recentemente anunciado continua sendo: " + listaOld[0][0] + " - " + listaOld[0][1])
-    
-    else:
-        print("O site retornou código de status: " + str(statuscodenumber))
+    listaOld = webscrap()
+    print("O item mais recentemente adicionado é: " + listaOld[0][0] + " - " + listaOld[0][1])
 
     time.sleep(timesleep)
+
+    while True:
+
+        screen_clear()
+
+        banner()
+        
+        statuscodenumber = statuscode()
+
+        if statuscodenumber == 200:
+            listaNew = webscrap()
+
+            if listaNew[0][0] != listaOld[0][0]:
+                print("O item mais recentemente anunciado é: " + listaNew[0][0] + " - " + listaNew[0][1])
+                
+                msg = EmailMessage()
+                msg['Subject'] = "[OLXCRAPPER] " + listaNew[0][0] + " - " + listaNew[0][1]
+                msg['From'] = EMAIL_ADDRESS
+                msg['To'] = EMAIL_ADDRESS
+                msg.set_content('Link para o anuncio: ' + listaNew[0][2])
+
+                with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                    smtp.send_message(msg)
+
+                listaOld = listaNew
+            
+            elif listaNew[0][0] == listaOld[0][0]:
+                print("O item mais recentemente anunciado continua sendo: " + listaOld[0][0] + " - " + listaOld[0][1])
+        
+        else:
+            print("O site retornou código de status: " + str(statuscodenumber))
+
+        time.sleep(timesleep)
+
+if __name__ == "__main__":
+	main()
